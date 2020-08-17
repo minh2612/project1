9cde6f8f4bd85ba77197b882960b93f9f10def65
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-17 17:47:16 +0700

    Update EmployeeController.php

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -183,7 +183,7 @@ class EmployeeController extends Controller
     );
         $data = array();
         $data['e_name'] = $request->e_name;
-       
+         
         $data['e_email'] = $request->e_email;
         $data['e_address'] = $request->e_address;
         $data['e_phone'] = $request->e_phone;
@@ -191,9 +191,23 @@ class EmployeeController extends Controller
         $data['department_id'] = $request->department_id;
         $data['position_id'] = $request->position_id;
         $data['e_password'] = md5($request->e_password);
+        $get_image = $request->file('e_avatar');
+        
+        if($get_image){
+                    $get_name_image = $get_image->getClientOriginalName();
+                    $name_image = current(explode('.',$get_name_image));
+                    $new_image =  $name_image.rand(0,99).'.'.$get_image->getClientOriginalExtension();
+                    $get_image->move('public/avatar',$new_image);
+                    $data['e_avatar'] = $new_image;
+                    DB::table('tbl_e')->where('e_id',$e_id)->update($data);
+                    Session::put('message','Cập nhật nhân viên thành công');
+                    return Redirect::to('all-employee');
+        }
+        else{
         DB::table('tbl_e')->where('e_id',$e_id)->update($data);
         Session::put('message','Cập nhật sản phẩm thành công');
         return Redirect::to('all-employee');
+    }
     }
 
     public function delete_employee($e_id){
