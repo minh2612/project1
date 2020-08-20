9a9396d996a0cdd6df1d33e555204ba88e61d142
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-20 09:01:31 -0700

    Update admin_layout.blade.php

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -90,6 +90,13 @@
                                     <li><a href="{{URL::to('/all-customer')}}">Danh sách khách hàng</a></li>
                                 </ul>
                             </li> 
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="dripicons-user-id"></i> <span> Quản lý nhóm khách hàng </span> <span class="menu-arrow float-right"><i class="mdi mdi-chevron-right"></i></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="{{URL::to('/add-customer-group')}}">Thêm nhóm khách hàng</a></li>
+                                    <li><a href="{{URL::to('/all-customer-group')}}">Danh sách nhóm khách hàng</a></li>
+                                </ul>
+                            </li> 
                         </ul>
                     </div>
                     <div class="clearfix"></div>
