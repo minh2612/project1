a21f30aaacf3a08382d8fbc46fda677f45e94448
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-20 08:48:52 +0700

    Update all_employee.blade.php

--- resources/views/all_employee.blade.php
+++ resources/views/all_employee.blade.php
@@ -22,34 +22,54 @@
             <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                 <thead>
                     <tr>
-                    	<th>ID</th>
+                    
+                        <th>Hành động</th>
                         <th>Tên nhân viên</th>
                         <th>Email</th>
-                        <th>Chức vụ</th>
-                        <th>Phòng ban</th>
-                        <th>Hành động</th>
+
+                        <th>Admin</th>
+                        <th>Quản lý</th>
+                        <th>Nhân viên</th>
+                   
+
                     </tr>
                 </thead>
                 <tbody>
-                @foreach($all_employee as $key => $employee)
-                    <tr>
-                        <td>#</td>
-                        <td>{{ $employee->e_name }}</td>
-                        <td>{{ $employee->e_email }}</td>
-                        <td>{{ $employee->position_name}}</td>
-                        <td>{{ $employee->department_name }}</td>
+                    
+                @foreach($all_employee as $key => $user)
+            <form action="{{url('/assign-roles')}}" method="POST">
+              @csrf
+              <tr>
+               
+                
+                <td>
+                     <a href="{{URL::to('/detail-employee/'.$user->e_id)}}" class="active styling-edit" ui-toggle-class="">
+                        <i class="fa fa-eye"></i>  
+                    <a href="{{URL::to('/edit-employee/'.$user->e_id)}}" class="active styling-edit" ui-toggle-class="">
+                        <i class="fa fa-edit"></i>
                      
-                        <td>
-                             <a href="{{URL::to('/detail-employee/'.$employee->e_id)}}" class="active styling-edit" ui-toggle-class="">
-                                <i class="fa fa-eye"></i>  
-                            <a href="{{URL::to('/edit-employee/'.$employee->e_id)}}" class="active styling-edit" ui-toggle-class="">
-                                <i class="fa fa-edit"></i>
-                             
-                            <a onclick="return confirm('Bạn có chắc là muốn xóa nhân viên này ko?')" href="{{URL::to('/delete-employee/'.$employee->e_id)}}" class="active styling-edit" ui-toggle-class="">
-                                <i class="fa fa-trash-alt"></i>
-                        </td>
-                    </tr>
-                @endforeach                   
+                    <a onclick="return confirm('Bạn có chắc là muốn xóa nhân viên này ko?')" href="{{URL::to('/delete-employee/'.$user->e_id)}}" class="active styling-edit" ui-toggle-class="">
+                        <i class="fa fa-trash-alt"></i>
+                </td>
+                <td>{{ $user->e_name }}</td>
+                <td>{{ $user->e_email }} <input type="hidden" name="e_email" value="{{ $user->e_email }}"   ></td>
+             
+                
+                <td><input type="checkbox" name="admin_role" {{$user->hasRole('admin') ? 'checked' : ''}}></td>
+                <td><input type="checkbox" name="manager_role"  {{$user->hasRole('manager') ? 'checked' : ''}}></td>
+                <td><input type="checkbox" name="user_role"  {{$user->hasRole('user') ? 'checked' : ''}}></td>
+
+              <td>
+                  
+                    
+                 <input type="submit" value="Cấp quyền" class="btn btn-sm btn-default">
+                
+              </td> 
+
+              </tr>
+            </form>
+          @endforeach     
+
                 </tbody>
             </table>
 
