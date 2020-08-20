9b640ea89632c7e35b054a7af5739046504118a4
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-20 10:54:30 +0700

    Update PositionController.php

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -1,38 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use DB;
 use Session;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\MessageBag;
-use Validator;
 session_start();
-
 class PositionController extends Controller
 {
-    public function AuthAdmin(){
-       $admin_id = Auth::user()->e_id;
-        $id=DB::table('tbl_e')->where('e_id',$admin_id)->first();
-         
-
-        if($admin_id && $id->is_admin==1){
-            return Redirect::to('admin-dashboard');
-        }else{
-            return Redirect::to('/')->send();
-        }
-    }
+  
 
     public function add_position()
     {
-        $this->AuthAdmin();
+        
         return view('add_position');     
     }
 
     public function all_position(){
-        //$this->AuthAdmin();
+      
         $all_position= DB::table('tbl_position')->get();
         $manager_position = view('all_position')->with('all_position', $all_position);
         return view('admin_layout')->with('all_position', $manager_position);
@@ -40,7 +26,6 @@ class PositionController extends Controller
     }
 
     public function save_position(Request $request){
-        $this->AuthAdmin();
      
         $data = array();
         $data['position_name']= $request->position_name;
@@ -71,9 +56,7 @@ class PositionController extends Controller
         }
     
      public function detail_position($position_id){
-        $this->AuthAdmin();
-
-        //$this->AuthLogin();
+        
         $detail_position = DB::table('tbl_position')->where('position_id',$position_id)->get();
         $detail_employee = DB::table('tbl_e')->where('position_id',$position_id)->get();
     
@@ -83,21 +66,22 @@ class PositionController extends Controller
 
     }
     public function edit_position($position_id){
-        $this->AuthAdmin();
+     
         $edit_position= DB::table('tbl_position')->where('position_id', $position_id)->get();
         $manager_position = view('edit_position')->with('edit_position', $edit_position);
         return view('admin_layout')->with('edit_position', $manager_position);
     }
     public function update_position(Request $request, $position_id){
-        $this->AuthAdmin();
+       
         $data = array();
         $data['position_name']= $request->position_name;
         DB::table('tbl_position')->where('position_id', $position_id)->update($data);
         Session::put('message', 'Cập nhật chức vụ thành công');
         return Redirect::to('all-position');
     }
+
     public function delete_position($position_id){
-        $this->AuthAdmin();
+       
         DB::table('tbl_position')->where('position_id', $position_id)->delete();
         Session::put('message', 'Xóa chức vụ thành công');
         return Redirect::to('all-position');
