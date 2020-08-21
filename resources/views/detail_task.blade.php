3ac9299082b66bd4bd7f1fe8e6209ed66169bf62
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-21 15:27:36 +0700

    Create detail_task.blade.php

---
+++ resources/views/detail_task.blade.php
@@ -0,0 +1,63 @@
+@extends('admin_layout')
+@section('admin_content')
+<div class="page-content-wrapper ">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="page-title-box">
+                    <div class="row align-items-center">
+                        <div class="col-md-8">
+                            <h4 class="page-title m-0">Chi tiết công việc</h4>
+                        </div>
+                        <!-- end col -->
+                    </div>
+                    <!-- end row -->
+                </div>
+                <!-- end page-title-box -->
+            </div>
+        </div> 
+        <!-- end page title -->     
+    <div class="table-responsive">
+            <table id="datatable" class="table table-hover " style="background-color: white; border-collapse: collapse; border-spacing: 0; width: 100%;">
+                
+                      
+                 @foreach($detail_task as $key => $task)
+                               
+                       <tr>
+                            <th>Tên công việc</th>  
+                            <th>{{ $task->task_name}}</th>
+                       </tr>
+                       <tr>
+                            <th>Tên dự án</th> 
+                            <th> 
+                               {{$task->project_name}}
+                            </th>
+                       </tr>
+                        <tr>
+                            <th>Người làm</th> 
+                            <th> @foreach($employee as $employee1)
+                               {{$employee1->e_name}}<br>
+                            @endforeach</th>
+                       </tr>
+                       <tr>
+                            <th>Ngày bắt đầu</th>  
+                            <th>{{ $task->task_start}}</th>
+                       </tr>
+                       <tr>
+                            <th>Ngày kết thúc</th>  
+                            <th>{{ $task->task_end}}</th>
+                       </tr>
+                        <tr>
+                            <th>Ghi chú</th>  
+                            <th>{{ $task->task_note}}</th>
+                       </tr>
+     
+                @endforeach                   
+            </table>
+           
+        </div>
+    </div><!-- container fluid -->
+
+</div> <!-- Page content Wrapper -->
+@endsection
