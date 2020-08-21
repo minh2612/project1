af85e85aed1f0b411030a0cea2aad873caf61d33
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-21 17:44:44 +0700

    Update add_task.blade.php

--- resources/views/add_task.blade.php
+++ resources/views/add_task.blade.php
@@ -70,6 +70,16 @@
                 <div class="col-sm-4">
                     <input class="form-control" type="date" name="task_end">
                 </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Mức độ ưu tiên</label>
+                <div class="col-sm-4">
+                    <select class="form-control" name="task_priority" >
+                        <option value="Thấp">Thấp</option>    
+                        <option value="Trung bình">Trung bình</option>    
+                        <option value="Cao">Cao</option>      
+                    </select>
+                </div>
             </div>			
 			<div class="form-group row">
                 <label for="example-date-input" class="col-sm-2 col-form-label">Ghi chú</label>
