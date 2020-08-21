23ea1951d4d0eb8145b687339826920989f4fde8
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-21 15:27:33 +0700

    Update detail_project.blade.php

--- resources/views/detail_project.blade.php
+++ resources/views/detail_project.blade.php
@@ -9,6 +9,14 @@
                     <div class="row align-items-center">
                         <div class="col-md-8">
                             <h4 class="page-title m-0">Chi tiết dự án</h4>
+                            <button href="{{URL::to('/add-task')}}" type="button" class="btn btn-success waves-effect waves-light">Thêm công việc</button>
+                            <?php
+                                $message= Session::get('message');
+                                 if($message){
+                                      echo '<span class="text-alert">'.$message.'</span>';
+                                      Session::put('message', null);
+                                    }
+                            ?>        
                         </div>
                         <!-- end col -->
                     </div>
@@ -18,42 +26,76 @@
             </div>
         </div> 
         <!-- end page title -->     
-    <div class="table-responsive">
-            <table id="datatable" class="table table-hover " style="background-color: white; border-collapse: collapse; border-spacing: 0; width: 100%;">
-                
-                      
-                 @foreach($detail_project as $key => $project)
-                               
-                       <tr>
-                            <th>Tên dự án</th>  
-                            <th>{{ $project>project_name}}</th>
-                       </tr>
-                        <tr>
-                            <th>Tên khách hàng</th>  
-                            <th>{{ $project->customer_name}}</th>
-                       </tr>
-                        <tr>
-                            <th>Người quản lý dự án</th>  
-                            <th>{{ $project->e_name}}</th>
-                       </tr>
-                        <tr>
-                            <th>Ngày bắt đầu</th>  
-                            <th>{{ $project->project_start}}</th>
-                       </tr>
-                        <tr>
-                            <th>Ngày kết thúc</th>  
-                            <th>{{ $project->project_end}}</th>
-                       </tr>
-                       <tr>
-                            <th>Các công việc cần làm</th>  
-                            <th>{{ $project->project_end}}</th>
-                       </tr> 
-                        
-                    
-                @endforeach                   
+        <div class="table-responsive">
+            <table id="datatable" class="table table-bordered" style="background-color: white; border-collapse: collapse; border-spacing: 0; width: 100%;">
+                <thead>
+                    <tr>
+                        <th>Tên công việc</th>
+                        <th>Tên dự án</th>
+                        <th>Người làm</th>
+                        <th>Deadline</th>
+                        <th>Ghi chú</th>
+                        <th>Trạng thái</th>
+                        <th>Hành động</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($all_task as $key => $task)
+                    <tr>
+            
+                        <td>{{$task->task_name}}</td>
+                        <td>
+                            {{$task->project_name}}<br>
+                        </td>
+                        <td>@foreach($employee as $key => $employee1) 
+                            @if($task->task_id==$employee1->task_id)
+                            {{$employee1->e_name}}<br>
+                            @endif
+                            @endforeach</td>            
+                        <td>{{$task->task_end}}</td>
+                        <td>{{$task->task_note}}</td>
+                        <td>
+                        <a href="{{URL::to('/detail-task/'.$task->task_id)}}" class="active styling-edit" ui-toggle-class="">
+                            <i class="fa fa-eye"></i>
+                        <a href="{{URL::to('/edit-task/'.$task->task_id)}}" class="active styling-edit" ui-toggle-class="">
+                            <i class="fa fa-edit"></i>
+                        <a onclick="return confirm('Bạn có chắc là muốn xóa công việc này ko?')" href="{{URL::to('/delete-task/'.$task->task_id)}}" class="active styling-edit" ui-toggle-class="">
+                            <i class="fa fa-trash-alt"></i>
+                        </td>
+                        <td>
+                            <span class="text-ellipsis">
+                            <?php
+                            if($task->task_status==0){
+                            ?>
+                                <a href="{{URL::to('/start-task/'.$task->task_id)}}"><button type="button"  style="width:150px;" class="btn btn-primary waves-effect waves-light">Chưa hoạt động</button></a>
+                            <?php
+                            }
+
+                            if($task->task_status==1){
+                            ?>
+                                <a href="{{URL::to('/submit-task/'.$task->task_id)}}"><button type="button" style="width:150px;" class="btn btn-info waves-effect waves-light">Đang làm</button></a>
+                            <?php
+                            }
+
+                            if($task->task_status==2){
+                            ?>
+                               <button type="button" style="width:150px;" class="btn btn-danger waves-effect waves-light">Đang đợi duyệt</button>
+                            <?php
+                            }
+
+                            if($task->task_status==3){
+                            ?>
+                                <button style="width:150px;" type="button" class="btn btn-success waves-effect waves-light">Hoàn tất</button>
+                            <?php
+                            }
+                            ?>
+                            </span>
+                        </td>      
+                    </tr>
+                @endforeach                 
                 </tbody>
             </table>
-           
+
         </div>
     </div><!-- container fluid -->
 
