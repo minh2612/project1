10b2550fba1466942e21b498f92725ad6123569c
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-22 08:45:02 +0700

    Create stack_user_task.blade.php

---
+++ resources/views/stack_user_task.blade.php
@@ -0,0 +1,61 @@
+@extends('admin_layout')
+@section('admin_content')
+<div class="page-content-wrapper ">
+
+    <div class="container-fluid">
+
+
+        <!-- end row -->
+        <div class="table-responsive">
+            <table class="table mb-0"><h5>Công việc đang chạy</h5>
+                <thead>
+                    <tr>                     
+                        <th>Dự án</th>
+                        <th>Tên công việc</th>
+                        <th>Người phụ trách</th>                        
+                        <th>Thành viên</th>
+                        <th>Ngày bắt đầu</th>
+                        <th>Ngày kết thúc</th>
+                        <th>Mức độ ưu tiên</th>
+
+                        <th>Trạng thái</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    @foreach($all_task as $key => $value)
+                        <th>{{$value->project_id}}</th>
+                        <th>{{$value->task_name}}</th>  
+                        <th>{{$value->e_name}}</th> 
+                       <th> @foreach($all_employee as $key)
+                            @if($key->task_id==$value->task_id)
+                               {{$key->e_name}}<br>
+                            @endif
+                        @endforeach</th>
+                         <th>{{$value->task_start}}</th> 
+                        <th>{{$value->task_end}}</th> 
+                          <th>{{$value->task_priority}}</th> 
+                        <td>
+                            <span class="text-ellipsis">
+                            <?php
+
+                            if($value->task_status==0){
+                            ?>
+                                <a href="{{URL::to('/submit-user-task/'.$value->task_id)}}"><button type="button" style="width:130px;" class="btn btn-warning waves-effect waves-light">Đang chờ</button></a>
+                            <?php
+                            }
+                            ?>
+                            </span>
+                        </td> 
+                    </tr>
+                    @endforeach
+                
+                   
+                </tbody>
+            </table>
+        </div>
+    </div><!-- container fluid -->
+
+</div> <!-- Page content Wrapper -->
+@endsection
