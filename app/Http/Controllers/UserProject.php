1f37231c18ebdbdec84b3b575b3dd77a15ff1b40
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-22 08:42:48 +0700

    Update UserProject.php

--- app/Http/Controllers/UserProject.php
+++ app/Http/Controllers/UserProject.php
@@ -26,78 +26,109 @@ class UserProject extends Controller
          public function loading_task(){
             $this->AuthUser();
              $status=1;
-            //$name=DB::table('tbl_e')->where('e_id',$id)->get();
+             $id=Auth::user()->e_id;
+           
              $all_task= DB::table('tbl_employee_task')
-             ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
-             ->join('tbl_project','tbl_project.project_id','=','tbl_employee_task.project_id')
-             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')->where('employee_id',$id)->where('tbl_task.task_status',$status) ->get();
+             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')
+              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
+             ->where('employee_id',$id)->where('tbl_task.task_status',$status)->get();
+          
 
            $all_employee=DB::table('tbl_employee_task')
              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')->get();
               
 
-             $manager_task = view('loading_task ')->with('all_task', $all_task)->with('all_employee',$all_employee);
-             return view('admin_layout')->with('$all_task', $manager_task);
+             $manager_task = view('loading_task')->with('all_task', $all_task)->with('all_employee',$all_employee);
+             return view('admin_layout')->with('loading_task', $manager_task);
            // return view('users_dashboard')->with('name',$name);
         }
 
         public function wait_user_task(){
-            $this->AuthUser();
-        $user_id = Session::get('e_id');
-             $id=Session::get('e_id');
+             $this->AuthUser();
              $status=2;
+             $id=Auth::user()->e_id;
             //$name=DB::table('tbl_e')->where('e_id',$id)->get();
              $all_task= DB::table('tbl_employee_task')
-             ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
-             ->join('tbl_project','tbl_project.project_id','=','tbl_employee_task.project_id')
-             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')->where('employee_id',$id)->where('tbl_task.task_status',$status) ->get();
+             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')
+              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
+             ->where('employee_id',$id)->where('tbl_task.task_status',$status)->get();
+          
 
            $all_employee=DB::table('tbl_employee_task')
              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')->get();
               
 
-             $manager_task = view('wait_user_task ')->with('all_task', $all_task)->with('all_employee',$all_employee);
-             return view('users_layout')->with('$all_task', $manager_task);
+             $manager_task = view('wait_user_task')->with('all_task', $all_task)->with('all_employee',$all_employee);
+             return view('admin_layout')->with('wait_user_task', $manager_task);
            // return view('users_dashboard')->with('name',$name);
         }
 
          public function end_user_task(){
             $this->AuthUser();
-        $user_id = Session::get('e_id');
-             $id=Session::get('e_id');
              $status=3;
+             $id=Auth::user()->e_id;
             //$name=DB::table('tbl_e')->where('e_id',$id)->get();
              $all_task= DB::table('tbl_employee_task')
-             ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
-             ->join('tbl_project','tbl_project.project_id','=','tbl_employee_task.project_id')
-             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')->where('employee_id',$id)->where('tbl_task.task_status',$status) ->get();
-
-           $all_employee=DB::table('tbl_employee_task')
-             ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')->get();
-              
-
-             $manager_task = view('end_user_task ')->with('all_task', $all_task)->with('all_employee',$all_employee);
-             return view('users_layout')->with('$all_task', $manager_task);
-           // return view('users_dashboard')->with('name',$name);
-        }
-
-        public function refuse_user_task(){
-            $this->AuthUser();
-        $user_id = Session::get('e_id');
-             $id=Session::get('e_id');
-             $status=4;
-            //$name=DB::table('tbl_e')->where('e_id',$id)->get();
-             $all_task= DB::table('tbl_employee_task')
-             ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
-             ->join('tbl_project','tbl_project.project_id','=','tbl_employee_task.project_id')
-             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')->where('employee_id',$id)->where('tbl_task.task_status',$status) ->get();
+             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')
+              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
+             ->where('employee_id',$id)->where('tbl_task.task_status',$status)->get();
+          
 
            $all_employee=DB::table('tbl_employee_task')
              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')->get();
               
 
              $manager_task = view('refuse_user_task')->with('all_task', $all_task)->with('all_employee',$all_employee);
-             return view('users_layout')->with('$all_task', $manager_task);
+             return view('admin_layout')->with('refuse_user_task', $manager_task);
+           // return view('users_dashboard')->with('name',$name);
+        }
+
+        public function refuse_user_task(){
+            $this->AuthUser();
+             $status=4;
+             $id=Auth::user()->e_id;
+            //$name=DB::table('tbl_e')->where('e_id',$id)->get();
+             $all_task= DB::table('tbl_employee_task')
+             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')
+              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
+             ->where('employee_id',$id)->where('tbl_task.task_status',$status)->get();
+          
+
+           $all_employee=DB::table('tbl_employee_task')
+             ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')->get();
+              
+
+             $manager_task = view('refuse_user_task')->with('all_task', $all_task)->with('all_employee',$all_employee);
+             return view('admin_layout')->with('refuse_user_task', $manager_task);
+           // return view('users_dashboard')->with('name',$name);
+        }
+         public function count_task(){
+            $id=Auth::user()->e_id;
+            $run_task=DB::table('tbl_employee_task')
+             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')
+             ->where('tbl_task.task_status',1)->where('employee_id',$id)->count();
+
+
+              return view('admin_layout')->with('run_task', $run_task);
+
+         }
+         public function stack_user_task(){
+            $this->AuthUser();
+             $status=0;
+             $id=Auth::user()->e_id;
+            //$name=DB::table('tbl_e')->where('e_id',$id)->get();
+             $all_task= DB::table('tbl_employee_task')
+             ->join('tbl_task','tbl_task.task_id','=','tbl_employee_task.task_id')
+              ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')
+             ->where('employee_id',$id)->where('tbl_task.task_status',$status)->get();
+          
+
+           $all_employee=DB::table('tbl_employee_task')
+             ->join('tbl_e','tbl_e.e_id','=','tbl_employee_task.employee_id')->get();
+              
+
+             $manager_task = view('refuse_user_task')->with('all_task', $all_task)->with('all_employee',$all_employee);
+             return view('admin_layout')->with('refuse_user_task', $manager_task);
            // return view('users_dashboard')->with('name',$name);
         }
 
