6b2f381d9cba593db45702456d3cfcb1351fc4b3
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-22 08:45:04 +0700

    Update wait_user_task.blade.php

--- resources/views/wait_user_task.blade.php
+++ resources/views/wait_user_task.blade.php
@@ -1,5 +1,5 @@
-@extends('users_layout')
-@section('users_content')
+@extends('admin_layout')
+@section('admin_content')
 <div class="page-content-wrapper ">
 
     <div class="container-fluid">
@@ -7,38 +7,42 @@
 
         <!-- end row -->
         <div class="table-responsive">
-            <table class="table mb-0"><h5>Công việc chờ duyệt</h5>
+            <table class="table mb-0"><h5>Công việc đang chạy</h5>
                 <thead>
                     <tr>                     
                         <th>Dự án</th>
-                        <th>Công việc</th>
+                        <th>Tên công việc</th>
                         <th>Người phụ trách</th>                        
                         <th>Thành viên</th>
                         <th>Ngày bắt đầu</th>
                         <th>Ngày kết thúc</th>
+                        <th>Mức độ ưu tiên</th>
+
                         <th>Trạng thái</th>
                         
                     </tr>
                 </thead>
+                <tbody>
                   <tr>
                     @foreach($all_task as $key => $value)
-                        <th>{{$value->project_name}}</th>
+                        <th>{{$value->project_id}}</th>
                         <th>{{$value->task_name}}</th>  
-                        <th>{{$value->task_admin}}</th> 
+                        <th>{{$value->e_name}}</th> 
                        <th> @foreach($all_employee as $key)
                             @if($key->task_id==$value->task_id)
                                {{$key->e_name}}<br>
                             @endif
                         @endforeach</th>
                          <th>{{$value->task_start}}</th> 
-                        <th>{{$value->task_end}}</th>   
+                        <th>{{$value->task_end}}</th> 
+                          <th>{{$value->task_priority}}</th> 
                         <td>
                             <span class="text-ellipsis">
                             <?php
 
                             if($value->task_status==2){
                             ?>
-                                <a href="{{URL::to('/submit-user-task/'.$value->task_id)}}"><button type="button" style="width:130px;" class="btn btn-danger waves-effect waves-light">Đang đợi duyệt</button></a>
+                                <a href="{{URL::to('/submit-user-task/'.$value->task_id)}}"><button type="button" style="width:130px;" class="btn btn-info waves-effect waves-light">Đang đợi duyệt</button></a>
                             <?php
                             }
                             ?>
@@ -46,7 +50,7 @@
                         </td> 
                     </tr>
                     @endforeach
-                <tbody>
+                
                    
                 </tbody>
             </table>
