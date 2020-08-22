ff9604a5f37b8d587b57d236bb2c495e7d508f67
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-22 08:43:13 +0700

    Update admin_dashboard.blade.php

--- resources/views/admin_dashboard.blade.php
+++ resources/views/admin_dashboard.blade.php
@@ -9,7 +9,7 @@
                 <div class="page-title-box">
                     <div class="row align-items-center">
                         <div class="col-md-8">
-                            <h4 class="page-title m-0">Danh sách công việc</h4>
+                            <h4 class="page-title m-0">Danh sách công việc ưu tiên</h4>
                         </div>
                         <!-- end col -->
                     </div>
@@ -20,81 +20,6 @@
         </div> 
 
         <!-- end page title -->     
-         <div class="row">
-            <div style="margin-right: 20px;">
-                <div class="card bg-primary mini-stat text-white" style="width:260px">
-                    <div class="p-3 mini-stat-desc">
-                        <div class="clearfix">
-                            <h6 class="text-uppercase mt-0 float-left text-white-50" style="color:black">Công việc đang chạy</h6>
-                            <h4 class="mb-3 mt-0 float-right">0</h4>
-                        </div>                     
-                    </div>
-                    <div class="p-3">
-                        <a href="{{URL::to('/loading-task')}}" style="color:white" class="font-14 m-0">Xem các công việc đang chạy</a>
-                    </div>
-                </div>
-            </div>
-
-             <div style="margin-right: 20px;">
-                <div class="card bg-info mini-stat text-white" style="width:260px">
-                    <div class="p-3 mini-stat-desc">
-                        <div class="clearfix">
-                            <h6 class="text-uppercase mt-0 float-left text-white-50">Công việc chờ duyệt</h6>
-                            <h4 class="mb-3 mt-0 float-right">0</h4>
-                        </div>
-                    </div>
-                    <div class="p-3">
-                        <a  href="{{URL::to('/refuse-user-task')}}" style="color:white" class="font-14 m-0">Xem các công việc chờ duyệt</a>
-                    </div>
-                </div>
-            </div>
-
-            <div style="margin-right: 20px;">
-                <div class="card bg-pink mini-stat text-white" style="width:260px">
-                    <div class="p-3 mini-stat-desc">
-                        <div class="clearfix">
-                            <h6 class="text-uppercase mt-0 float-left text-white-50">Công việc bị từ chối</h6>
-                            <h4 class="mb-3 mt-0 float-right">0</h4>
-                        </div>
-                    </div>
-                    <div class="p-3">
-                        <a href="{{URL::to('/wait-user-task')}}" style="color:white" class="font-14 m-0">Xem các công việc bị từ chối</a>
-                    </div>
-                </div>
-            </div>
-            
-           
-
-            <div style="margin-right: 20px;">
-                <div class="card  mini-stat text-white" style="width:260px; background-color: #ff9800;">
-                    <div class="p-3 mini-stat-desc">
-                        <div class="clearfix">
-                            <h6 class="text-uppercase mt-0 float-left text-white-50">Công việc treo</h6>
-                            <h4 class="mb-3 mt-0 float-right">0</h4>
-                        </div>
-                    </div>
-                    <div class="p-3">
-                        <a  href="{{URL::to('/end-user-task')}}" style="color:white" class="font-14 m-0">Xem các công việc treo</a>
-                    </div>
-                </div>
-            </div>
-
-            <div style="margin-right: 20px;">
-                 <div class="card bg-success mini-stat text-white" style="width:260px">
-                    <div class="p-3 mini-stat-desc">
-                        <div class="clearfix">
-                            <h6 class="text-uppercase mt-0 float-left text-white-50">Công việc hoàn thành</h6>
-                            <h4 class="mb-3 mt-0 float-right">0</h4>
-                        </div>
-                    </div>
-                    <div class="p-3">
-                        <a  href="{{URL::to('/end-user-task')}}" style="color:white" class="font-14 m-0">Xem các công việc đã hoàn thành</a>
-                    </div>
-                </div>
-            </div>
-
-          
-        </div>  
 
 </div> 
 </div>
@@ -105,6 +30,7 @@
                         <th>STT</th>
                         <th>Tên công việc</th>
                         <th>Dự án</th>
+                         <th>Độ ưu tiên</th>
                         <th>Ngày bắt đầu</th>
                         <th>Ngày kết thúc</th>
                         <th>Ghi chú</th>
@@ -127,6 +53,7 @@
                         <td>{{ $project1->project_name }}</td>
                         @endif
                         @endforeach
+                        <td>{{ $task->task_priority}}</td>
                         <td>{{ $task->task_start}}</td>
                         <td>{{ $task->task_end}}</td>
                         <td>{{ $task->task_note}}</td>
