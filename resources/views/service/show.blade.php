1641573383b7f36a4a2a753e16749d60e945457e
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-25 17:39:42 +0700

    Update show.blade.php

--- resources/views/service/show.blade.php
+++ resources/views/service/show.blade.php
@@ -11,7 +11,7 @@
                             <h4 class="page-title m-0">Danh sách dịch vụ</h4>
                             <p>&nbsp;</p>
                             <a href="{{URL::to('/add-service/')}}" class="active styling-edit" ui-toggle-class="">
-                            <i class="fas fa-plus-circle fa-2x"></i></a> 
+                            <button type="button" class="btn btn-success waves-effect waves-light">Thêm dịch vụ</button></a> 
                             <p>&nbsp;</p>
                             <?php
                                 $message= Session::get('message');
