b2b3ab1997013b2c9060a9599235b3c0ac6a5323
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-25 17:39:36 +0700

    Update show.blade.php

--- resources/views/customer_group/show.blade.php
+++ resources/views/customer_group/show.blade.php
@@ -11,7 +11,7 @@
                             <h4 class="page-title m-0">Danh sách nhóm khách hàng</h4>
                             <p>&nbsp;</p>
                             <a href="{{URL::to('/add-customer-group/')}}" class="active styling-edit" ui-toggle-class="">
-                            <i class="fas fa-plus-circle fa-2x"></i></a> 
+                            <button type="button" class="btn btn-success waves-effect waves-light">Thêm nhóm khách hàng</button></a> 
                             <p>&nbsp;</p>
                             <?php
                                 $message= Session::get('message');
