6c6fa9a7a5695b624cb06fccd8df8b82880eca48
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-25 16:37:35 +0700

    Update DepartmentController.php

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,46 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
-use Auth;
 use Session;
+use Carbon\Carbon;
 use App\Http\Requests;
+use Auth;
 use Illuminate\Support\Facades\Redirect;
 session_start();
 
 class DepartmentController extends Controller
 {
-   public function AuthLogin(){
-            $admin_id = Auth::id();
-            if($admin_id){
-                return Redirect::to('admin-dashboard');
-            }else{
-                return Redirect::to('/')->send();
-            }
+    public function AuthLogin(){
+        $admin_id = Auth::id();
+        if($admin_id){
+            return Redirect::to('admin-dashboard');
+        }else{
+            return Redirect::to('/')->send();
         }
-
-    public function add_department()
-    {
-       $this->AuthLogin();
-        return view('add_department');     
     }
 
-    public function all_department(){
-         $this->AuthLogin();
-        $all_department= DB::table('tbl_department')->get();
-        $manager_department = view('all_department')->with('all_department', $all_department);
-        return view('admin_layout')->with('all_department', $manager_department);
+    public function add()
+    {
+        $this->AuthLogin();
+        return view('department.add');     
+    }
 
+    public function show(){
+        $this->AuthLogin();
+        $department= DB::table('tbl_department')->get();
+        $show = view('department.show')->with('department', $department);
+        return view('admin_layout')->with('department', $show);
         }
 
-    public function save_department(Request $request){
-         $this->AuthLogin();
-        $data = array();
-        $data['department_name']= $request->department_name;
-        $data['department_note']= $request->department_note;
-              $this->validate($request,
+    public function save(Request $request){
+        $this->AuthLogin();
+        $this->validate($request,
         [
                        
-            'department_name' => 'bail|required|unique:tbl_department',
+            'name' => 'bail|required|unique:tbl_department,department_name',
             
             
                 
@@ -56,50 +53,53 @@ class DepartmentController extends Controller
 
         [
             
-            'department_name' => 'Tên phòng ban',
-      ]
+            'name' => 'Tên chức vụ',
+        ]
 
     );
-        DB::table('tbl_department')->insert($data);
-        Session::put('message', 'Thêm phòng ban thành công');
-        return Redirect::to('all-department');
-    }
-
-   public function detail_department($department_id){
-     
-
-        $this->AuthLogin();
-        $detail_department = DB::table('tbl_department')->where('department_id',$department_id)->get();
-        $detail_employee = DB::table('tbl_e')->where('department_id',$department_id)->get();
-    
-        
-        $manager_department  = view('detail_department')->with('detail_department',$detail_department)->with('detail_employee',$detail_employee);
-        return view('admin_layout')->with('detail_department', $manager_department);
-
-    }
-    public function edit_department($department_id){
-         $this->AuthLogin();
-        $edit_department= DB::table('tbl_department')->where('department_id', $department_id)->get();
-        $manager_department = view('edit_department')->with('edit_department', $edit_department);
-        return view('admin_layout')->with('edit_department', $manager_department);
-        Textarea::get('description');
-    }
-
-    public function update_department(Request $request, $department_id){
-         $this->AuthLogin();
         $data = array();
-        $data['department_name']= $request->department_name;
-        $data['department_note']= $request->department_note;
+        $data['department_name']= $request->name;
+        $data['department_note']= $request->note;
+        DB::table('tbl_department')->insert($data);
+        Session::put('message', 'Thêm chức vụ thành công');
+        return Redirect::to('all-department');
+    }
+
+    public function edit($department_id){
+        $this->AuthLogin();
+        $department= DB::table('tbl_department')->where('department_id', $department_id)->get();
+        $show= view('department.edit')->with('department', $department);
+        return view('admin_layout')->with('department.edit', $show);
+    }
+
+    public function update(Request $request, $department_id){
+        $this->AuthLogin();
+        $this->validate($request,
+        [                 
+            'name' => 'bail|required',     
+        ],
+
+        [
+            'required' => ':attribute không được để trống',
+        ],
+
+        [
+            'name' => 'Tên chức vụ',
+        ]
+
+    );
+        $data = array();
+        $data['department_name']= $request->name;
+        $data['department_note']= $request->note;
         DB::table('tbl_department')->where('department_id', $department_id)->update($data);
-        Session::put('message', 'Cập nhật phòng ban thành công');
+        Session::put('message', 'Cập nhật chức vụ thành công');
         return Redirect::to('all-department');
     }
 
-    public function delete_department($department_id){
-       $this->AuthLogin();
+    public function delete($department_id){
+        $this->AuthLogin();
         DB::table('tbl_department')->where('department_id', $department_id)->delete();
-        Session::put('message', 'Xóa phòng ban thành công');
+        Session::put('message', 'Xóa chức vụ thành công');
         return Redirect::to('all-department');
     }
-
 }
