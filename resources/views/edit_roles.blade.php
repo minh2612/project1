3d544cbf4366d204e5580b1db006e5411823d2f1
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-26 17:37:50 +0700

    Update edit_roles.blade.php

--- resources/views/edit_roles.blade.php
+++ resources/views/edit_roles.blade.php
@@ -28,94 +28,203 @@
   
 		   	
 			<form role="form" method="post" action="{{URL::to('/update-roles/'.$role->id_roles)}}" enctype="multipart/form-data">
-            {{csrf_field()}}        
-            <div class="form-group row">
-                <label for="example-name-input" class="col-sm-2 col-form-label">Tên vai trò</label>
+            {{csrf_field()}}     
+           
+
+                           
+           
+               
+               <div  class="form-group row" style="padding-left: 20px;">
+                <label for="example-date-input" style="padding-left: 20px;" class="col-sm-2 col-form-label">Tên vai trò</label>
                 <div class="col-sm-4">
-                    <input class="form-control" value="{{ $role->name }}" type="text" name="name" id="example-name-input">
+                    <input class="form-control" type="text" name="name" id="today">
                 </div>
             </div>
-            
-             <div style="margin-left: 20px; margin-top: 50px;" >
-            <h6 style="padding-bottom: 10px;" class="page-title m-0">VAI TRÒ</h6>
+               
+           <div class="form-group row"  style="padding-left: 20px;">
+                <label for="example-date-input" style="padding-left: 20px;" class="col-sm-2 col-form-label">Mô tả</label>
+                <div class="col-sm-4">
+                    <input class="form-control" type="text" name="roles_note" id="today">
+                </div>
+            </div>
+            <div class="form-group row"  style="padding-left: 20px;">
+                <label for="example-date-input" style="padding-left: 20px;" class="col-sm-2 col-form-label">Chọn quyền</label>
+            </div>
+
+         <div style="margin-left: 20px;   width: 400px;" class="container float-left">
+            <h6 style="padding-bottom:10px; " class="page-title m-0"><input id="checkall1" class='' type="checkbox"> <span class="badge badge-success">VAI TRÒ</span> </h6>
             @foreach($permission  as $permission1)
                 @if(strpos($permission1->permission_name,'role'))
-                    <div class="form-check" style="float: left; padding-right: 100px;">
-                        <input type="checkbox" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
+                    <div class="form-check" >
+                        <input type="checkbox" class="1" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
                         <label class="form-check-label" > {{trans('auth.'.$permission1->permission_name)}}</label>
                     </div>
                 @endif
                 @endforeach
         </div> 
 
-         <div style="margin-left: 20px; margin-top: 50px;" >
-            <h6 style="padding-bottom: 10px;" class="page-title m-0">NHÂN VIÊN</h6>
+        
+         <div style="margin-left: 20px;   width: 400px;" class="container float-left" >
+            <h6 style="padding-bottom:10px; " class="page-title m-0"><input id="checkall2" class='' type="checkbox"> <span class="badge badge-success">NHÂN VIÊN</span> </h6>
+            
             @foreach($permission  as $permission1)
                 @if(strpos($permission1->permission_name,'employee'))
-                    <div class="form-check" style="float: left; padding-right: 100px;">
-                       <input type="checkbox" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
+                <div class="form-check" >
+                        <input type="checkbox" class="2" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
                         <label class="form-check-label" > {{trans('auth.'.$permission1->permission_name)}}</label>
                     </div>
                 @endif
             @endforeach    
         </div>  
 
-         <div style="margin-left: 20px; margin-top: 50px;" >
-            <h6 style="padding-bottom: 10px;" class="page-title m-0">DỰ ÁN</h6>
+         <div style="margin-left: 20px;   width: 400px;" class="container float-left">
+            <h6 style="padding-bottom:10px; " class="page-title m-0"><input id="checkall3" class='' type="checkbox"> <span class="badge badge-success">DỰ ÁN</span> </h6>
             @foreach($permission  as $permission1)
                 @if(strpos($permission1->permission_name,'project'))
-                    <div class="form-check" style="float: left; padding-right: 100px;">
-                        <input type="checkbox" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
+                    <div class="form-check" >
+                        <input type="checkbox" class="3" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
                         <label class="form-check-label" > {{trans('auth.'.$permission1->permission_name)}}</label>
                     </div>
                 @endif
-            @endforeach    
+                @endforeach
         </div> 
 
-         <div style="margin-left: 20px; margin-top: 50px;" >
-            <h6 style="padding-bottom: 10px; " class="page-title m-0">CÔNG VIỆC</h6>
-                
+         <div style="margin-left: 20px; margin-top: 30px;  width: 400px;" class="container float-left">
+            <h6 style="padding-bottom:10px; " class="page-title m-0"><input id="checkall4" class='' type="checkbox"> <span class="badge badge-success">CÔNG VIỆC</span> </h6>
             @foreach($permission  as $permission1)
                 @if(strpos($permission1->permission_name,'task'))
-                    <div class="form-check" style="float:left; padding-right: 100px;">
-                        <input type="checkbox" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
-                        <label  class="form-check-label" > {{trans('auth.'.$permission1->permission_name)}}</label>
-                    </div>
-                
-                @endif
-            @endforeach
-
-        </div> 
-
-
-         <div style="margin-left: 20px; margin-top: 50px;" >
-            <h6 style="padding-bottom:10px; padding-top: 15px;" class="page-title m-0">CHỨC VỤ</h6>
-            @foreach($permission  as $permission1)
-                @if(strpos($permission1->permission_name,'position'))
-                    <div class="form-check" style="float: left; padding-right: 100px;">
-                       <input type="checkbox" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
+                    <div class="form-check" >
+                        <input type="checkbox" class="4" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
                         <label class="form-check-label" > {{trans('auth.'.$permission1->permission_name)}}</label>
                     </div>
+                @endif
+                @endforeach
+        </div> 
+   
+
+         
+
+         
+
+
+         <div style="margin-left: 20px; margin-top: 30px;  width: 400px;" class="container float-left"  >
+            <h6 style="padding-bottom:10px; " class="page-title m-0"><input id="checkall5" class='' type="checkbox"> <span class="badge badge-success">CHỨC VỤ</span> </h6>
+            @foreach($permission  as $permission1)
+                @if(strpos($permission1->permission_name,'position'))
+                    <span ><div class="form-check" >
+                        <input type="checkbox" class="5" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
+                        <label class="form-check-label" > {{trans('auth.'.$permission1->permission_name)}}</label>
+                    </div></span>
                 @endif
             @endforeach    
         </div>    
                
-        <div style="margin-left: 20px; margin-top: 50px;" >
-            <h6 style="padding-bottom: 10px;" class="page-title m-0">PHÒNG BAN</h6>
+        <div  style="margin-left: 20px; margin-top: 30px;  width: 400px;" class="container float-left">
+            <h6 style="padding-bottom:10px; " class="page-title m-0"><input id="checkall6" class='' type="checkbox"><span class="badge badge-success"> PHÒNG BAN</span></h6>
             @foreach($permission  as $permission1)
                 @if(strpos($permission1->permission_name,'department'))
-                    <div class="form-check" style="float: left; padding-right: 100px;">
-                        <input type="checkbox" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">  
+                    <span><div class="form-check" >
+                        <input type="checkbox" class="6" name="permission[]"  {{ $getAllPermissionOfRole->contains($permission1->id_permission) ? 'checked' : ''}} value="{{$permission1->id_permission }}">
                         <label class="form-check-label" > {{trans('auth.'.$permission1->permission_name)}}</label>
-                    </div>
+                    </div></span>
                 @endif
-            @endforeach    
-        </div>    
+            @endforeach  
+            </div>
+          
               
-			<button type="submit" name="update_department" class="btn btn-success waves-effect waves-light">Cập nhật phòng ban</button>
-		</form>
+              <ul>
+               @foreach ($errors->all() as $error)
+                    <li>{{$error }}</li>
+               @endforeach
+            </ul>
+                      
+                 <button  style="margin-right: 800px; margin-top: 80px;" type="submit" name="add_position" class=" float-left btn btn-success waves-effect waves-light">Cập nhật</button>        
+        </form>
+        
+        </div> 
 
-    </div><!-- container fluid -->
+        </div>   
 
-</div> <!-- Page content Wrapper -->
+        
+        <script type="text/javascript">
+                 $("#checkall1").click(function (){
+             if ($("#checkall1").is(':checked')){
+                $(".1").each(function (){
+                   $(this).prop("checked", true);
+                   });
+                }else{
+                   $(".1").each(function (){
+                        $(this).prop("checked", false);
+                   });
+                }
+         });
+                 </script>
+                 <script type="text/javascript">
+                 $("#checkall2").click(function (){
+             if ($("#checkall2").is(':checked')){
+                $(".2").each(function (){
+                   $(this).prop("checked", true);
+                   });
+                }else{
+                   $(".2").each(function (){
+                        $(this).prop("checked", false);
+                   });
+                }
+         });
+                 </script>
+                 <script type="text/javascript">
+                 $("#checkall3").click(function (){
+             if ($("#checkall3").is(':checked')){
+                $(".3").each(function (){
+                   $(this).prop("checked", true);
+                   });
+                }else{
+                   $(".3").each(function (){
+                        $(this).prop("checked", false);
+                   });
+                }
+         });
+                 </script>
+                 <script type="text/javascript">
+                 $("#checkall4").click(function (){
+             if ($("#checkall4").is(':checked')){
+                $(".4").each(function (){
+                   $(this).prop("checked", true);
+                   });
+                }else{
+                   $(".4").each(function (){
+                        $(this).prop("checked", false);
+                   });
+                }
+         });
+                 </script>
+                 <script type="text/javascript">
+                 $("#checkall5").click(function (){
+             if ($("#checkall5").is(':checked')){
+                $(".5").each(function (){
+                   $(this).prop("checked", true);
+                   });
+                }else{
+                   $(".5").each(function (){
+                        $(this).prop("checked", false);
+                   });
+                }
+         });
+                 </script>
+                 <script type="text/javascript">
+                 $("#checkall6").click(function (){
+             if ($("#checkall6").is(':checked')){
+                $(".6").each(function (){
+                   $(this).prop("checked", true);
+                   });
+                }else{
+                   $(".6").each(function (){
+                        $(this).prop("checked", false);
+                   });
+                }
+         });
+                 </script>
+  
+
+ <!-- Page content Wrapper -->
 @endsection
