593f5294c4d44f7c4520f4d04ab28e787d8b4541
laptrinhkalzen <68837856+laptrinhkalzen@users.noreply.github.com>
2020-08-28 17:47:57 +0700

    Update add.blade.php

--- resources/views/customer/add.blade.php
+++ resources/views/customer/add.blade.php
@@ -1,9 +1,8 @@
 @extends('admin_layout')
 @section('admin_content')
- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 <div class="page-content-wrapper ">
     <div class="container-fluid">
         <div class="row">
